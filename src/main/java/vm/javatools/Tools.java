8816d17023fd390d07d61371b5904678b6dc0fc5
xmic <xmic@fi.muni.cz>
2023-07-04 05:02:55 +0200

    maximum paralelisation

--- src/main/java/vm/javatools/Tools.java
+++ src/main/java/vm/javatools/Tools.java
@@ -12,7 +12,7 @@ import java.util.logging.Logger;
  */
 public class Tools {
 
-    public static final Integer PARALELISATION = Math.min(Math.max(1, Runtime.getRuntime().availableProcessors() - 1), 31);
+    public static final Integer PARALELISATION = Math.max(1, Runtime.getRuntime().availableProcessors() - 1);
 //    public static final Integer PARALELISATION = 20;
     public static final Logger LOG = Logger.getLogger(Tools.class.getName());
 
