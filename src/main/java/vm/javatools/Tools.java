58492be7bc8377d6702e4bb21ffca23221c7b760
xmic <xmic@fi.muni.cz>
2023-07-06 03:37:27 +0200

    Update Tools.java

--- src/main/java/vm/javatools/Tools.java
+++ src/main/java/vm/javatools/Tools.java
@@ -12,7 +12,7 @@ import java.util.logging.Logger;
  */
 public class Tools {
 
-    public static final Integer PARALELISATION = Math.max(1, (int) (Runtime.getRuntime().availableProcessors() / 3f));
+    public static final Integer PARALELISATION = Math.max(1, Runtime.getRuntime().availableProcessors() - 1);
 //    public static final Integer PARALELISATION = 14;
     public static final Logger LOG = Logger.getLogger(Tools.class.getName());
 
